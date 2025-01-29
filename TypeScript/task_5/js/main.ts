523023c26d45b144eec5d26ca79387f7bff756a7
Ema Decot <emadecot03@gmail.com>
2025-01-29 19:01:09 +0100

    add task 5

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -8,3 +8,25 @@ interface MinorCredits {
   brand: "MinorCredits";
 }
 
+// additionner les MajorCredits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MajorCredits" };
+}
+
+// additionner les MinorCredits
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MinorCredits" };
+}
+
+// exemple
+const major1: MajorCredits = { credits: 30, brand: "MajorCredits" };
+const major2: MajorCredits = { credits: 20, brand: "MajorCredits" };
+
+const minor1: MinorCredits = { credits: 10, brand: "MinorCredits" };
+const minor2: MinorCredits = { credits: 5, brand: "MinorCredits" };
+
+const resultMajor = sumMajorCredits(major1, major2);
+const resultMinor = sumMinorCredits(minor1, minor2);
+
+console.log("Total Major Credits:", resultMajor);
+console.log("Total Minor Credits:", resultMinor);
