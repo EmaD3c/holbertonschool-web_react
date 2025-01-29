39b282394f5f848d879071075ff5cc7a781e1e0d
Ema Decot <emadecot03@gmail.com>
2025-01-29 12:05:47 +0100

    add task 6 btask 7 and file for task 8

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -49,3 +49,34 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));  // teacher
 console.log(createEmployee(1000)); // director
 console.log(createEmployee('$500')); // director
+
+// check if an employee is a director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// function that execute work based on employee type
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// exemple
+executeWork(createEmployee(200));  // "Getting to work"
+executeWork(createEmployee(1000)); // "Getting to director tasks"
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects) {
+  if ( todayClass == "Math") {
+    console.log("Teaching Math");
+  } else {
+    console.log("Teaching History");
+  }
+}
+
+console.log(teachClass("Math")); // "Teaching Math"
+console.log(teachClass("History")); // "Teaching History"
